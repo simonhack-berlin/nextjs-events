27f7b60092dab45073053d3abba84df59b68444e
Simone Capuano <scapuano19@gmail.com>
2022-10-31 11:11:48 +0100

    added events search (wip) - at moment is without logic

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,23 @@
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
 import EventList from '../../components/events/events-list';
+import EventsSearch from '../../components/events/events-search';
 import { getAllEvents } from '../../dummy-data';
 
 function AllEvents() {
+    const router = useRouter();
     const events = getAllEvents();
 
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    }
+
     return (
-        <div>
+        <Fragment>
+            <EventsSearch onSearch={findEventsHandler}/>
             <EventList events={events} />
-        </div>
+        </Fragment>
     )
 }
 
