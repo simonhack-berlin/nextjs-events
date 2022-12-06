7907e32555e6e7d67d756b4ed327b8dea3bc95f9
Simone Capuano <scapuano19@gmail.com>
2022-12-06 17:40:12 +0100

    Head tag added - added Head tag from next/head with title and description

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 import Layout from '../components/layout/layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Next Events</title>
+        <meta name="description" content="find a lot of great events" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.1" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
